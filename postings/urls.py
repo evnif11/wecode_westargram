605542bc77e0563f762f9eafec71ecaef43c5eb6
Chanjoo lee <dlckswn14@gmail.com>
2021-12-26 17:57:01 +0900

    엔드포인트 수정 및 생성

--- postings/urls.py
+++ postings/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path
 
-from postings.views import CommentView, LikeView, LikersView, PostingView
+from postings.views import CommentView, FollowView, FollowerListView, FollowingListView, LikeView, LikersView, PostingView, UnfollowView
 
 urlpatterns = [
-    path('', PostingView.as_view()),
-    path('/<int:posting_id>/comments', CommentView.as_view()),
-    path('/<int:posting_id>/like', LikeView.as_view()),
-    path('/<int:posting_id>/likers', LikersView.as_view()),
+    path('postings', PostingView.as_view()),
+    path('postings/<int:posting_id>/comments', CommentView.as_view()),
+    path('postings/<int:posting_id>/like', LikeView.as_view()),
+    path('postings/<int:posting_id>/likers', LikersView.as_view()),
+    path('follow', FollowView.as_view()),
+    path('unfollow', UnfollowView.as_view()),
+    path('users/<int:user_id>/follower', FollowerListView.as_view()),
+    path('users/<int:user_id>/following', FollowingListView.as_view()),
 ]
--- westargram/urls.py
+++ westargram/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 
 urlpatterns = [
+    path('', include('postings.urls')),
     path('users', include('users.urls')),
-    path('postings', include('postings.urls')),
 ]
